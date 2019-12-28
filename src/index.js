fdd1f358763bcd5edfd8dea4764834f150d5e46c
jstiven01 <stiven10@gmail.com>
2019-12-28 07:10:56 -0500

    Creating functions that takes a location and return weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import weather from './weather'
 console.log("Installing the Basic Setup");
+weather.takeLocation('London');
+weather.getWeatherLocation();
+weather.takeLocation('Bogota');
+weather.getWeatherLocation();
+weather.takeLocation('Nairobi');
+weather.getWeatherLocation();
---
+++ src/weather.js
@@ -0,0 +1,28 @@
+const weatherAPI = (() => {
+    let location;
+    const url = 'http://api.openweathermap.org/data/2.5/weather?q=';
+    const API = 'c09b7b383de915e8d54efbee3298eb08';
+    const takeLocation = (place) => {
+        location = place;
+    }
+    const getWeatherLocation = async () => {
+        console.log(setUrl());
+        try {
+            const response = await fetch(setUrl(), {mode: 'cors'});
+            const weatherData = await response.json();
+            console.log(weatherData, setUrl());
+        } catch (error) {
+            console.log({error});   
+        }
+    }
+    const setUrl = () => {
+        return `${url}${location}&appid=${API}`
+    }
+
+    return {
+        takeLocation, getWeatherLocation
+    }
+
+})();
+
+export default weatherAPI;
