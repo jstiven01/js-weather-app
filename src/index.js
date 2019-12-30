2ace3259f8a47f52da109abd0edc07c1f4129749
jstiven01 <stiven10@gmail.com>
2019-12-30 11:29:37 -0500

    Updating Readme and organizing files into folders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'bootstrap';
-import './style.scss';
-import weather from './weather';
+import './css/style.scss';
+import weather from './js/weather';
 
 const submit = document.getElementById('submit');
 submit.addEventListener('click', weather.getWeatherLocation);
